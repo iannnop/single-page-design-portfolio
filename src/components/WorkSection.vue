ef582f2960dedadfb4ba2e864560f12091cdd0cd
ian <iannnop@gmail.com>
2022-08-09 20:57:01 -0500

    work section

---
+++ src/components/WorkSection.vue
@@ -0,0 +1,30 @@
+<script>
+import Carousel from "./carousel/Carousel.vue";
+
+export default {
+  components: {
+    Carousel
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h2>My Work</h2>
+    <Carousel />
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  color: var(--black);
+  font-size: 24px;
+  line-height: 30px;
+  font-weight: 700;
+}
+
+div {
+  margin: 100px 0;
+  text-align: center;
+}
+</style>
