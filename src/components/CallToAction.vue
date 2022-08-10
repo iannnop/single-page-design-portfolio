4a30d99c6318e9c72f2aa499e4a7b37e409cf34c
ian <iannnop@gmail.com>
2022-08-09 20:56:49 -0500

    call to action section

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,31 @@
+<script>
+import ButtonColored from "./ButtonColored.vue"
+
+export default {
+  components: {
+    ButtonColored
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h2>Book a call with me</h2>
+    <p>I'd love to have a chat to see how I can help you. The best first step is for us to discuss your project during a free consultation. Then we can move forward from there.</p>
+    <ButtonColored />
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: var(--black);
+  color: var(--light-cream);
+  border-radius: 10px;
+  padding: 48px 24px;
+  text-align: center;
+}
+
+p {
+  margin: 24px 0;
+}
+</style>
