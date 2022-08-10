ce68e74115dce8931ec989a2482205638981b835
ian <iannnop@gmail.com>
2022-08-09 20:56:07 -0500

    buttons

---
+++ src/components/ButtonBlack.vue
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<template>
+  <button class="btn btn-black">Free Consultation</button>
+</template>
+
+<style scoped>
+.btn {
+  border: none;
+  padding: 8px 28px;
+  background-color: transparent;
+  border-radius: 28px;
+  font-weight: 700;
+  line-height: 28px;
+  font-size: 14px;
+}
+
+.btn-black {
+  background-color: var(--black);
+  color: var(--light-cream);
+}
+
+.btn-black:hover {
+  background-color: var(--galactic-blue);
+  cursor: pointer;
+}
+
+</style>
---
+++ src/components/ButtonColored.vue
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<template>
+  <button class="btn btn-colored">Free Consultation</button>
+</template>
+
+<style scoped>
+.btn {
+  border: none;
+  padding: 8px 28px;
+  background-color: transparent;
+  border-radius: 28px;
+  font-weight: 700;
+  line-height: 28px;
+  font-size: 14px;
+}
+
+.btn-colored {
+  background-color: #EB7565;
+  color: var(--light-cream);
+}
+
+.btn-colored:hover {
+  background-color: #F6A560;
+  cursor: pointer;
+}
+
+</style>
